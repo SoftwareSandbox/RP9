2a98c4d23fb15fd9f2a1ee223c40f9957928ac5c
vercruyssetim <vercruysse.tim@gmail.com>
2019-05-29 18:37:08 +0200

    - added spring boot and banner

--- root-module/src/main/java/com/cegeka/rp9/root/module/Application.java
+++ root-module/src/main/java/com/cegeka/rp9/root/module/Application.java
@@ -1,12 +1,20 @@
 package com.cegeka.rp9.root.module;
 
 import com.cegeka.rp9.secondary.module.World;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public class Application {
+@SpringBootApplication
+public class Application implements CommandLineRunner {
 
     public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
         var worldMessage = new World().getWorldMessage();
         System.out.println("Hello " + worldMessage);
     }
-
 }
--- root-module/src/main/java/module-info.java
+++ root-module/src/main/java/module-info.java
@@ -1,3 +1,6 @@
-module root.module {
+open module root.module {
     requires secondary.module;
+
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
 }
