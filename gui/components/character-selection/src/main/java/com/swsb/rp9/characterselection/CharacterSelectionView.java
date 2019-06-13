59bea370771c2aea70fb009a9f32b029fb083c20
nielsdelestinne <nielsdelestinne@gmail.com>
2019-06-13 21:53:50 +0200

    Niels, Jasper: Fix css url for CharacterSelectionView and add Start Game button.

--- gui/components/character-selection/src/main/java/com/swsb/rp9/characterselection/CharacterSelectionView.java
+++ gui/components/character-selection/src/main/java/com/swsb/rp9/characterselection/CharacterSelectionView.java
@@ -2,9 +2,12 @@ package com.swsb.rp9.characterselection;
 
 import com.swsb.rp9.core.Dimension;
 import com.swsb.rp9.core.GameView;
+import com.swsb.rp9.core.TransitionSlot;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
@@ -33,7 +36,7 @@ public class CharacterSelectionView extends GameView {
 
     @Override
     public String getStyleSheetLocation() {
-        return getClass().getResource("com/swsb/rp9/characterselection/styles/character-selection.css").toExternalForm();
+        return getClass().getResource("/com/swsb/rp9/characterselection/styles/character-selection.css").toExternalForm();
     }
 
     @Override
@@ -42,9 +45,21 @@ public class CharacterSelectionView extends GameView {
     }
 
     private Parent createCharacterSelectionScreen() {
-        TilePane characterScreen = new TilePane();
+        TilePane characterScreen = new TilePane(createStartGameButton());
         characterScreen.setBackground(new Background(new BackgroundFill(Color.ORANGE, CornerRadii.EMPTY, Insets.EMPTY)));
         characterScreen.setAlignment(Pos.CENTER);
         return characterScreen;
+    }
+
+    private Button createStartGameButton() {
+        Button startGameButton = new Button("Start game");
+        startGameButton.setOnKeyPressed(
+                event -> {
+                    if (KeyCode.ENTER.equals(event.getCode())) {
+                        registerTransitionSlot(TransitionSlot.TRANSITION_SLOT_ONE);
+                    }
+                }
+        );
+        return startGameButton;
     }
 }
