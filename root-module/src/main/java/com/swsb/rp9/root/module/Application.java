b7776022809035066a100b397474bfc579c80fd8
Jasper Hawinkel <jasper.hawinkel@cegeka.com>
2019-06-13 23:29:07 +0200

    Jasper: Intentionally break build to test slack notifications

--- root-module/src/main/java/com/swsb/rp9/root/module/Application.java
+++ root-module/src/main/java/com/swsb/rp9/root/module/Application.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application implements CommandLineRunner {
 
     public static void main(String[] args) {
+        whoopsie
         SpringApplication.run(Application.class, args);
     }
 
