7cc21d191977c6abd415b760bcf8bd4fee5f02b0
Jasper Hawinkel <jasper.hawinkel@cegeka.com>
2019-06-13 23:30:59 +0200

    Revert "Jasper: Intentionally break build to test slack notifications"
    This reverts commit b7776022

--- root-module/src/main/java/com/swsb/rp9/root/module/Application.java
+++ root-module/src/main/java/com/swsb/rp9/root/module/Application.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application implements CommandLineRunner {
 
     public static void main(String[] args) {
-        whoopsie
         SpringApplication.run(Application.class, args);
     }
 
